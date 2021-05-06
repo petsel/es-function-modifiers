aa8513ade70230ee9465982e651eb8f8e9b90f8d
Peter Seliger <peter.seliger@deutschebahn.com>
2021-05-06 20:09:57 +0200

    refactor existing type definitions. be more specific/accurate with the existing ones, introduce additional `modified` types. Finally provide new type definitions for the coming implementation of `afterReturning`.

--- src/typedef.js
+++ src/typedef.js
@@ -1,39 +1,93 @@
 /**
- * Any modified function's `around` handler (or callback) which enables
- * the interception of such a modified function's control flow.
+ * Any modified function type's `around` handler (or callback) which enables the
+ * interception and generic manipulation of the original function's control flow.
  *
- * Every `around` callback gets passed 3 arguments, `proceed` which is the
- * original/unmodified function, `handler` which is the very callback's own
- * reference and `argumentArray`, an `Array` type which holds any passed argument.
+ * Every `around` handler gets passed 3 arguments, 1st `proceed` which is the
+ * original function, 2nd `handler` which is the `around` handler's own reference.
+ * And 3rd `argumentArray`, an `Array` type which holds any passed argument.
  *
  * @callback aroundHandler
  *  @param {Function} proceed - The original/unmodified function/method.
- *  @param {Function} handler - The callback's own reference.
+ *  @param {Function} handler - The `around` handler's own reference.
  *  @param {Array} argumentArray - An `Array` type which holds any passed argument.
  */
 
 /**
- * Any modified function's `before` handler (or callback) which enables to hook
- * into such a modified function's control flow before the original function
- * is going to be invoked.
+ * The modified function type itself. The return value of any modified function
+ * of this type depends on the implementation of its own (injected) `aroundHandler`.
  *
- * Every `before` callback gets passed exactly one argument, the `argumentArray`,
- * an `Array` type which holds any passed argument.
+ * Thus only a prototypal `around` or `aroundModifier` enable the interception and
+ * generic manipulation of the original function's control flow.
+ *
+ * @callback aroundType
+ *  @returns {*}
+ *   Custom return value which depends on any `aroundHandler`'s specific implementation.
+ */
+
+/**
+ * Any modified function type's `before` handler (or callback)
+ * which will be invoked before the original function was called.
+ *
+ * Every `before` handler gets passed exactly one argument.
+ * The `argumentArray`, an `Array` type which holds any passed argument.
  *
  * @callback beforeHandler
  *  @param {Array} argumentArray - An `Array` type which holds any passed argument.
  */
 
 /**
- * Any modified function's `after` handler (or callback) which enables to hook
- * into such a modified function's control flow after the original function
- * has been invoked.
+ * The modified function type itself. Every modified function of this type returns
+ * the original function's return value.
  *
- * Every `around` callback gets passed 2 arguments, `result` which is the
- * return value of the before invoked original function and `argumentArray`,
- * an `Array` type which holds any passed argument.
+ * @callback beforeType
+ *  @returns {*} - The original function's return value.
+ */
+
+/**
+ * Any modified function type's `after` handler (or callback)
+ * which will be invoked after the original function was called.
+ *
+ * Every `after` handler gets passed 2 arguments. 1st `result` which is the
+ * return value of the before invoked original function.
+ * And 2nd `argumentArray`, an `Array` type which holds any passed argument.
  *
  * @callback afterReturningHandler
  *  @param {*} result - The return value of the before invoked original function.
  *  @param {Array} argumentArray - An `Array` type which holds any passed argument.
+ */
+
+/**
+ * The modified function type itself. Every modified function of this type returns
+ * the original function's return value.
+ *
+ * @callback afterReturningType
+ *  @returns {*} - The original function's return value.
+ */
+
+/**
+ * Any modified function type's `afterThrowing` handler (or callback) which will
+ * be invoked after the original function was called, regardless of its result.
+ *
+ * Every `afterThrowing` handler gets passed 2 arguments. 1st `result` which is
+ * either the catched exception, raised by having called the original function
+ * or the return value of the latter.
+ * And 2nd `argumentArray`, an `Array` type which holds any passed argument.
+ *
+ * @callback afterThrowingHandler
+ *  @param {(Error|*)} result
+ *   Either the catched exception, raised by having called the original function
+ *   or the return value of the latter.
+ *  @param {Array} argumentArray
+ *   An `Array` type which holds any passed argument.
+ */
+
+/**
+ * The modified function type itself. Every modified function of this type returns
+ * either the catched exception, raised by having called the original function
+ * or the return value of the latter.
+ *
+ * @callback afterThrowingType
+ *  @returns {(Error|*)}
+ *   Either the catched exception, raised by having called the original function
+ *   or the return value of the latter.
  */
