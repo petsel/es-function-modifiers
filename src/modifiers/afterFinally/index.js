71c2e018927640b22c4c1204e22b9d96691d8c68
Peter Seliger <peter.seliger@deutschebahn.com>
2021-05-06 23:06:07 +0200

    Provide 5th modifier implementation ... `afterFinally`.

---
+++ src/modifiers/afterFinally/index.js
@@ -0,0 +1,93 @@
+import isFunction from '../../utils/type-detection';
+import getSanitizedTarget from '../../utils/sanitizing';
+
+// provide *"prototype ready"* implementation.
+
+/**
+ * A prototypal implementation of any `Function` type's `afterFinally` modifier/behavior.
+ *
+ * Once available/enabled as `Function.prototype.afterFinally`, any `Function` type can be
+ * directly invoked via e.g. `[target.]myFunctionOrMethod.afterFinally(handler[, target])`.
+ *
+ * @param {afterFinallyHandler} handler - The callback/hook provided as `afterFinally` handler.
+ * @param {*=} target
+ *  The optional `target` which should be applicable as a method's *context*.
+ *  It will be sanitized/casted to either an applicable type or to the `null` value.
+ *
+ * @returns {(afterFinallyType|*)}
+ *  Returns either the modified function/method or, in case of any failure, does
+ *  return the context it was invoked at (which too is expected to be a `Function` type).
+ */
+export function afterFinally(handler, target) {
+  target = getSanitizedTarget(target);
+
+  const proceed = this;
+  // prettier-ignore
+  return (
+
+    isFunction(handler) &&
+    isFunction(proceed) &&
+
+    function afterFinallyType(...argumentArray) {
+      // the target/context of the initial modifier/modification time
+      // still can be overruled by a handler's apply/call time context.
+      const context = getSanitizedTarget(this) ?? target;
+
+      let result;
+      let error;
+      try {
+        // try the invocation of the original function.
+
+        result = proceed.apply(context, argumentArray);
+
+      } catch (exception) {
+
+        error = exception;
+
+      } // finally { ... }
+      /**
+       *  This is a design choice in order to ensure the consistent handling
+       *  of (intercepted) arguments in how any handler/callback gets passed
+       *  such arguments regardless of the concrete modifier implementation.
+       *
+       *  Never `apply` the arguments, but always provide
+       *  them within/as a single array of arguments.
+       *
+       *  nope ... --`handler.apply(context, [result, ...argumentArray]);`--
+       *  nope ... --`handler.call(context, result, ...argumentArray);`--
+       *
+       *  yep ... **`handler.call(context, result, argumentArray);`**
+       */
+      result = (error || result);
+
+      handler.call(context, result, argumentArray);
+
+      // always ensure a return value, ...
+      // ... either the original method's/function's return value or an error type.
+      return result;
+    }
+  ) || proceed;
+}
+afterFinally.toString = () => 'afterFinally() { [native code] }';
+
+// provide static implementation as well.
+
+/**
+ * Kind of a static utility implementation of any `Function` type's `afterFinally` modifier/behavior.
+ *
+ * Two `Function` types need to be passed to `afterFinallyModifier` like ...
+ * `afterFinallyModifier(proceed, handler[, target])`.
+ *
+ * @param proceed - The original/unmodified function/method.
+ * @param {afterFinallyHandler} handler - The callback/hook provided as `afterFinally` handler.
+ * @param {*=} target
+ *  The optional `target` which should be applicable as a method's *context*.
+ *  It will be sanitized/casted to either an applicable type or to the `null` value.
+ *
+ * @returns {(afterFinallyType|*)}
+ *  Returns either the modified function/method or, in case of any failure,
+ *  does return whatever was passed as this function's 1st argument.
+ */
+export function afterFinallyModifier(proceed, handler, target) {
+  return afterFinally.call(proceed, handler, target);
+}
